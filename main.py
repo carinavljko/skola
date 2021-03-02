cd8ee988442b5e672c97d4501e181c63b0284aa2
real-GuyWithANerfGun <79905640+real-GuyWithANerfGun@users.noreply.github.com>
2021-03-02 11:02:17 +0100

    haha

---
+++ main.py
@@ -0,0 +1,60 @@
+__author__ = 'veljko'
+#this is just a small game don't actually base a mail service to this
+#oh yes and the name is a joke on MSN
+
+print("Mycrows Secure Networking - Mail Edition")
+print("")
+print("MSN - ME")
+
+print("")
+
+print("|X\X\  /X/X|    /=======|    |X\X\    |X|         ")
+print("| |\ \/ /|X|    | |          | |\ \   | |         ")
+print("|X| \XX/ |X|    \=======\    |X| \X\  |X|  ME     ")
+print("| |  \/  |X|            |    | |  \ \ | |         ")
+print("|X|      |X|    |=======/    |X|   \XX|X|    1998™")
+print("")
+
+print("What is username?")
+user_name = input()
+print("What is password?")
+password = input()
+if user_name == "vljko":
+    if password == "tchmn":
+        print("Successfully logged in.")
+        print("")
+        print("Inbox - tchmnvljko@hotmail.com")
+        print("")
+        print("guywithanerfgun@hotmail.com - I need that python script NOW!!!")
+        print("ogisha@hotmail.com - yoo bro i think the boss is mad at u")
+        print("worknewsletter@workmail.com - Email deletes mail after a week")
+        print("guywithanerfgun@hotmail.com - Did you finish the python script?")
+        print("freeps5@gmail.com - CLICK HERE FOR A FREE PS5!!!")
+        print("ogisha@hotmail.com - yo wanna play cod4 sometime?")
+        print("youtubeteam091@gmail.com - Your youtube acconut it will be delted")
+        print("")
+        print("")
+        print("~End of email list~")
+        print("")
+
+    else:
+        print("Incorrect Log in information, restart application to retry.")
+
+if user_name == "primerzaskolu":
+    if password == "haha":
+        print("Successfully logged in.")
+        print("")
+        print("Inbox - primerzaa@hotmail.com")
+        print("")
+        print("veljkotiragic@hotmail.com - Ja sam napravio ovaj program :)")
+        print("haha")
+        print("veljko")
+        print("je")
+        print("napravio")
+        print("ovu")
+        print("aplikaciju")
+        print("")
+        print("")
+        print("~stranica jedan od pedeset osam~")
+
+input("Press Enter key to end the session")
